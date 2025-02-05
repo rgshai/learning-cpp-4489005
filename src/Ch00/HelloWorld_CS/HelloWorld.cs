046ef13828865417987722f6aa7a0ed821b7a5e5
Shai Goldberg <shai.goldberg@philips.com>
2025-02-05 11:59:41 +0000

    Commit Test

---
+++ src/Ch00/HelloWorld_CS/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld_VSCode
+{
+    class Program 
+    {
+        static void main()
+        {
+            System.Console.WriteLine("Heloo World");
+        }
+    }
+}
